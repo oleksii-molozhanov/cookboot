a5f78d30bc47fbce9901caa7f598500c06fced28
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-06 19:37:10 +0200

    Add handler for a single recipe

--- src/cookboot/adapters/http/recipe.py
+++ src/cookboot/adapters/http/recipe.py
@@ -10,3 +10,9 @@ class RecipeResource:
         resp.status = fl.HTTP_200  # This is the default status
         resp.content_type = fl.MEDIA_TEXT  # Default is JSON, so override
         resp.text = "Sweet stuff"
+
+    async def on_get_recipe(self, req: fl.Request, resp: fl.Response, recipe_id: str):
+        """Handles GET requests"""
+        resp.status = fl.HTTP_200  # This is the default status
+        resp.content_type = fl.MEDIA_TEXT  # Default is JSON, so override
+        resp.text = f"Salty stuff: {recipe_id}"
--- src/cookboot/app.py
+++ src/cookboot/app.py
@@ -3,4 +3,6 @@ import falcon.asgi
 from adapters.http.recipe import RecipeResource
 
 app = application = falcon.asgi.App()
-app.add_route("/recipes", RecipeResource())
+recipes = RecipeResource()
+app.add_route("/recipes", recipes)
+app.add_route("/recipes/{recipe_id}", recipes, suffix="recipe")

d10d038317adad0ea4226e1150e82e6d47798c41
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-07 16:46:49 +0200

    Factor out creation of the falcon app

--- src/cookboot/app.py
+++ src/cookboot/app.py
@@ -1,7 +1,12 @@
 import falcon.asgi
 from adapters.http.recipe import RecipeResource
 
-app = falcon.asgi.App()
-recipes = RecipeResource()
-app.add_route("/recipes", recipes)
-app.add_route("/recipes/{recipe_id}", recipes, suffix="recipe")
+
+# Having this as a function simplifies testing
+def create_app() -> falcon.asgi.App:
+    app = falcon.asgi.App()
+    recipes = RecipeResource()
+    app.add_route("/recipes", recipes)
+    app.add_route("/recipes/{recipe_id}", recipes, suffix="recipe")
+
+    return app

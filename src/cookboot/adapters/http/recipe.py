737f11cccf52043d394f628fb295f7793d37fab4
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-07 16:37:09 +0200

    Add post request handler

--- src/cookboot/adapters/http/recipe.py
+++ src/cookboot/adapters/http/recipe.py
@@ -1,7 +1,6 @@
-from ast import Call
-from urllib import response
 import falcon as fl
 from typing import Callable
+import json
 
 
 class RecipeResource:
@@ -9,10 +8,14 @@ class RecipeResource:
         """Handles GET requests"""
         resp.status = fl.HTTP_200  # This is the default status
         resp.content_type = fl.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = "Sweet stuff"
+        resp.text = "List of all recipes"
 
     async def on_get_recipe(self, req: fl.Request, resp: fl.Response, recipe_id: str) -> Callable:
         """Handles GET requests"""
         resp.status = fl.HTTP_200  # This is the default status
         resp.content_type = fl.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = f"Salty stuff: {recipe_id}"
+        resp.text = f"Tasty stuff: {recipe_id}"
+
+    async def on_post(self, req: fl.Request, resp: fl.Response) -> Callable:
+        data = json.loads(await req.stream.readall())
+        resp.status = fl.HTTP_200

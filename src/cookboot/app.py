4c303e974aa43c72754da059444cd2ea35366279
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-07 16:36:57 +0200

    remove useless variable

--- src/cookboot/app.py
+++ src/cookboot/app.py
@@ -1,8 +1,7 @@
 import falcon.asgi
-
 from adapters.http.recipe import RecipeResource
 
-app = application = falcon.asgi.App()
+app = falcon.asgi.App()
 recipes = RecipeResource()
 app.add_route("/recipes", recipes)
 app.add_route("/recipes/{recipe_id}", recipes, suffix="recipe")

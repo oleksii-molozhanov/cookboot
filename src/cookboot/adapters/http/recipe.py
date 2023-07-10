c988d995312cedb58291d48734cc02f4d4daf16a
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-10 17:17:57 +0200

    Fix mypy typing issues

--- src/cookboot/adapters/http/recipe.py
+++ src/cookboot/adapters/http/recipe.py
@@ -1,21 +1,25 @@
 import falcon as fl
-from typing import Callable
 import json
+from cookboot.application.recipe_book import RecipeBook
 
 
 class RecipeResource:
-    async def on_get(self, req: fl.Request, resp: fl.Response) -> Callable:
+    def __init__(self, recipe_book: RecipeBook) -> None:
+        self._recipe_book = recipe_book
+
+    async def on_get(self, req: fl.Request, resp: fl.Response) -> None:
         """Handles GET requests"""
         resp.status = fl.HTTP_200  # This is the default status
         resp.content_type = fl.MEDIA_TEXT  # Default is JSON, so override
         resp.text = "List of all recipes"
 
-    async def on_get_recipe(self, req: fl.Request, resp: fl.Response, recipe_id: str) -> Callable:
+    async def on_get_recipe(self, req: fl.Request, resp: fl.Response, recipe_id: str) -> None:
         """Handles GET requests"""
         resp.status = fl.HTTP_200  # This is the default status
         resp.content_type = fl.MEDIA_TEXT  # Default is JSON, so override
         resp.text = f"Tasty stuff: {recipe_id}"
 
-    async def on_post(self, req: fl.Request, resp: fl.Response) -> Callable:
+    async def on_post(self, req: fl.Request, resp: fl.Response) -> None:
         data = json.loads(await req.stream.readall())
+
         resp.status = fl.HTTP_200
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,17 @@
-from shutil import ExecError
 import pytest
 import falcon.testing
 import falcon as fl
+from typing import Type, no_type_check
 from cookboot.app import create_app
 
 
 @pytest.fixture
-def client():
+def client() -> falcon.testing.TestClient:
     app = create_app()
     return falcon.testing.TestClient(app)
 
 
+@no_type_check  # Disable mypy since VC Code still can't properly invoke it acknowledging --excludes
 def test_recipe_get_should_succeed(client):
     # given when
     resp = client.simulate_get("/recipes")
@@ -21,6 +22,7 @@ def test_recipe_get_should_succeed(client):
     assert resp.text == "List of all recipes"
 
 
+@no_type_check
 def test_recipe_get_recipe_id_should_succeed(client):
     # given
     recipe_id = "avocado_yummy"
@@ -32,3 +34,12 @@ def test_recipe_get_recipe_id_should_succeed(client):
     assert resp.status_code == 200
     assert resp.headers["content-type"] == fl.MEDIA_TEXT
     assert resp.text == f"Tasty stuff: {recipe_id}"
+
+
+@no_type_check
+def test_app_get_unknown_route_should_fail(client):
+    # given when
+    resp = client.simulate_get("/non_existing_route")
+
+    # then
+    assert resp.status_code == 404

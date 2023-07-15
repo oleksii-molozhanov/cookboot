0ecae6704e7fbb7b619e0fc7cbb2bc5e0fb9b679
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-15 11:58:54 +0200

    Add stab implementation

--- src/cookboot/application/recipe_book_impl.py
+++ src/cookboot/application/recipe_book_impl.py
@@ -1,6 +1,6 @@
-from uuid import UUID, uuid4
-from typing import Type
-from cookboot.domain.recipe import Recipe
+from typing import Union
+from cookboot.domain.recipe import Recipe, RecipeId, Ingridient
+from cookboot.domain.user import UserId
 from cookboot.application.recipe_repo import RecipeRepo
 
 
@@ -8,8 +8,16 @@ class RecipeBookImpl:
     """Implementation of RecipeBook protocol."""
 
     def __init__(self, repo: RecipeRepo) -> None:
-        # this will be used for now instead of a direct query to repo
         self._repo = repo
 
-    def add_new_recipe(self, name: str, ingridients: list[str]) -> Recipe:
-        return self._repo.create(name, ingridients)
+    def add_new_recipe(self, name: str, ingridients: list[Ingridient], user: UserId) -> Recipe:
+        """Creates a new recipe, stores it in the repo and returns it."""
+        return self._repo.create(name, ingridients, user)
+
+    def get_recipe(self, id: RecipeId, user: UserId) -> Union[Recipe, None]:
+        """Returns a recipe from repo by RecipeId for the given UserId.
+        Returns None if a recipe is not found."""
+
+    def list_all(self, user: UserId) -> list[Recipe]:
+        """Returns all recipes of the user."""
+        return []

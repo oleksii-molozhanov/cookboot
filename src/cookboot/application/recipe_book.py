5f6f76581ac9f9b7a245872965ef31acc8aaa81e
Oleksii <71426795+oleksii-molozhanov@users.noreply.github.com>
2023-07-15 11:52:47 +0200

    Adjust RecipeBook interface

--- src/cookboot/application/recipe_book.py
+++ src/cookboot/application/recipe_book.py
@@ -1,7 +1,16 @@
+from typing import Union, runtime_checkable
 from typing import Protocol
-from cookboot.domain.recipe import Ingridient, Recipe
+from cookboot.domain.recipe import Ingridient, Recipe, RecipeId
+from cookboot.domain.user import UserId
 
 
+@runtime_checkable
 class RecipeBook(Protocol):
-    def add_new_recipe(self, name: str, ingridients: list[Ingridient]) -> Recipe:
+    def add_new_recipe(self, name: str, ingridients: list[Ingridient], user: UserId) -> Recipe:
+        ...
+
+    def get_recipe(self, id: RecipeId, user: UserId) -> Union[Recipe, None]:
+        ...
+
+    def list_all(self, user: UserId) -> list[Recipe]:
         ...
